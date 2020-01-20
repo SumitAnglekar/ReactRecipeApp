bf39f591decffb89ae99be61f7b5ea76b729b4bf
Sumit_Anglekar <anglekar.s@husky.neu.edu>
2020-01-20 13:16:40 -0500

    Basic react app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+
+  const APP_ID = "80c17640";
+  const APP_KEY = "3ae3b8ebc50f02035df01fc046f8c0cd";
+  const ex = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+  
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() =>{
+    //console.log("Effect has been run");
+    getRecipes();
+  },[]);
+
+  const getRecipes = async () => {
+    const response= await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data = await response.json();
+    console.log(data);
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="search-form">
+        <input className="search-bar" type="text"/>
+        <button
+          className="search-button" 
+          type="submit">submit</button>
+          <h1 onClick={() => setCounter(counter+1)}>{counter}</h1>
+      </form>
     </div>
   );
 }
