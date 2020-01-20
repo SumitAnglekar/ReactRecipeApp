50b2c9ba47a98af69c30d00067bdd465e5fd7a51
Sumit_Anglekar <anglekar.s@husky.neu.edu>
2020-01-20 14:11:05 -0500

    creating a recipe component and accessing the api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, {useEffect, useState} from "react";
+import Recipe from './Recipe';
 import "./App.css";
 
 const App = () => {
 
   const APP_ID = "80c17640";
   const APP_KEY = "3ae3b8ebc50f02035df01fc046f8c0cd";
-  const ex = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+  //const ex = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
   
-  const [counter, setCounter] = useState(0);
+  const [recipes, setRecipes] = useState([]);
+
+  //const [counter, setCounter] = useState(0);
 
   useEffect(() =>{
     //console.log("Effect has been run");
@@ -17,18 +20,22 @@ const App = () => {
   const getRecipes = async () => {
     const response= await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
     const data = await response.json();
-    console.log(data);
-  }
+    setRecipes(data.hits);
+    console.log(data.hits);
+  };
   
   return (
     <div className="App">
       <form className="search-form">
         <input className="search-bar" type="text"/>
         <button
-          className="search-button" 
-          type="submit">submit</button>
-          <h1 onClick={() => setCounter(counter+1)}>{counter}</h1>
+          className="search-button" type="submit">Search</button>
       </form>
+      {recipes.map(recipe =>(
+        <Recipe title={recipe.recipe.label} 
+        calories={recipe.recipe.calories}
+        image={recipe.recipe.image}  />
+      ))}
     </div>
   );
 }
